4b54265dae1969d1ad99975e8bd4a02211988e79
Zhang Jie <2001213008@stu.pku.edu.cn>
2021-11-11 14:17:41 +0800

    add rsign and rsign_tb, test passed

--- rsign.sv
+++ rsign.sv
@@ -5,30 +5,22 @@ module rsign #(
     input   wire                clk,
     input   wire                rstn,
     input   wire                data_in_valid,
-    input   wire                mode_in,        // Load parameter when low, calculate when asserted
     input   wire signed [15:0]  para_in [FM_DEPTH-1:0],
     input   wire signed [15:0]  data_in [FM_DEPTH-1:0][CORE_SIZE-1:0],
-    output  reg  signed         data_out[FM_DEPTH-1:0][CORE_SIZE-1:0],
-    output  reg                 data_out_valid
+    output  reg                 data_out[FM_DEPTH-1:0][CORE_SIZE-1:0]
 );
 genvar i, j;
 generate 
     for(i = 0; i < FM_DEPTH; i = i + 1) begin: Rsign_for
-        for(j = 0; j< 9; j = j + 1) begin
-            always @(posedge clk or negedge rst_n) begin
-                if(~rstn) begin    // When RESET is valid, reset data_out, data_e_out
+        for(j = 0; j< CORE_SIZE; j = j + 1) begin
+            always @(posedge clk or negedge rstn) begin
+                if(~rstn)
                     data_out[i][j] <= 1'b0;
-                    data_out_valid <= 0;
-                end 
                 else begin
-                    if(data_in_valid) begin
-                        data_out[i][j] <= data_in[i][j] > para_in[i] ? 1'b1 : 1'b0;
-                        data_out_valid <= 1;
-                    end 
-                    else begin
+                    if(data_in_valid)
+                        data_out[i][j] <= (data_in[i][j] > para_in[i]) ? 1'b1 : 1'b0;
+                    else
                         data_out[i][j] <= data_out[i][j];
-                        data_out_valid <= 0;
-                    end//if
                 end
             end //always
         end // for j
