7be3fa870e1ddc1a1af0bdc395d3ac98a82bb2f9
Zhang Jie <2001213008@stu.pku.edu.cn>
2021-11-11 21:24:05 +0800

    Add bn_res module and tb, problem unsolved

--- bn_res.sv
+++ bn_res.sv
@@ -1,44 +1,54 @@
-module bn_res_layer4
+module bn_res
 #(
     parameter DATA_WIDTH   = 16,
     parameter PARA_WIDTH   = 16,
-    parameter CHANNEL_NUM  = 128
+    parameter CHANNEL_NUM  = 128,
+	parameter FM_DEPTH	   = 64
 )
 (
     // GLOBAL SIGNALS
     input   wire                    			clk             ,    			// System Clock
     input   wire                    			rstn            ,    			// System Reset, Active LOW
     input   wire                    			data_in_valid   ,    			// DATA Enable signal, Active HIGH
-    input   wire                    			mode_in         ,
     input   wire signed [PARA_WIDTH - 1 : 0] 	bn_a     [CHANNEL_NUM - 1 : 0],	// Parameter of BN
     input   wire signed [PARA_WIDTH - 1 : 0] 	bn_b     [CHANNEL_NUM - 1 : 0],	// Parameter of BN
-    input   wire signed [DATA_WIDTH - 1 : 0] 	res      [CHANNEL_NUM - 1 : 0],	// Residual
-    input   wire signed [DATA_WIDTH - 1 : 0] 	data_in  [CHANNEL_NUM - 1 : 0],	// Data from partial_sum
+    input   wire signed [DATA_WIDTH - 1 : 0] 	res      [FM_DEPTH - 1    : 0],	// Residual
+    input   wire signed [5 : 0] 				data_in  [CHANNEL_NUM - 1 : 0],	// Data from partial_sum
     output  wire signed [DATA_WIDTH - 1 : 0] 	data_out [CHANNEL_NUM - 1 : 0],	// Data to RPReLU
     output  reg                     			data_out_valid              	// DATA Enable signal to RPReLU
 );
 
-reg signed [PARA_WIDTH + DATA_WIDTH : 0] product [CHANNEL_NUM - 1 : 0];
+reg  signed [PARA_WIDTH + DATA_WIDTH -1: 0] product [CHANNEL_NUM - 1 : 0];
+wire signed [DATA_WIDTH - 1 : 0] 			data_in_extended [CHANNEL_NUM - 1 : 0];
+wire signed [PARA_WIDTH + DATA_WIDTH -1: 0]	product_sr [CHANNEL_NUM - 1 : 0];
 
 genvar i;
 generate
 	for(i = 0; i < CHANNEL_NUM; i = i + 1) begin
+		assign data_in_extended[i] = {{10{data_in[i][5]}}, data_in[i]};
 		always @(posedge clk or negedge rstn) begin    
 	        if(~rstn) begin
 	            data_out_valid  <= 0;
 	            product[i] 		<= 0;
-	        end else if(mode_in && data_in_valid) begin
-	            data_out_valid  <= 1;
-	            product[i] 		<= bn_a[i] * data_in[i];
-	        end else begin
-	            data_out_valid  <= 0;
-	            product[i] 		<= data_out[i];
+	        end 
+			else begin
+				if(data_in_valid) begin
+	            	data_out_valid  <= 1;
+	            	product[i] 		<= bn_a[i] * data_in_extended[i];
+	        	end 
+				else begin
+	            	data_out_valid  <= 0;
+	            	product[i] 		<= product[i];
+				end
 	        end
 	    end
-		if (i < CHANNEL_NUM/2)
-			assign data_out[i] = {{product[i] + bn_b[i]} >> 8 } + res[i];
+
+		assign product_sr[i] = product[i] >>> 8;
+
+		if (i < FM_DEPTH)
+			assign data_out[i] = product_sr[i] + bn_b[i]  + res[i];
 		else
-			assign data_out[i] = {product[i] + bn_b[i]} >> 8;
+			assign data_out[i] = product_sr[i] + bn_b[i];
 	end
 endgenerate
 
---
+++ bn_res_tb.sv
@@ -0,0 +1,83 @@
+`timescale 1ns/1ns
+module bn_res_tb #(
+	parameter DATA_WIDTH   = 16,
+	parameter PARA_WIDTH   = 16,
+	parameter CHANNEL_NUM  = 128,
+	parameter FM_DEPTH = 64
+) (
+);
+
+reg 						clk, rstn, data_in_valid;
+reg  [PARA_WIDTH - 1 : 0] 	bn_a     [CHANNEL_NUM - 1 : 0];
+reg  [PARA_WIDTH - 1 : 0] 	bn_b     [CHANNEL_NUM - 1 : 0];
+reg  [DATA_WIDTH - 1 : 0] 	res      [FM_DEPTH - 1 : 0];
+reg  [5 : 0]				data_in  [CHANNEL_NUM-1:0];
+wire [DATA_WIDTH - 1 : 0]	data_out [CHANNEL_NUM-1:0];
+wire						data_out_valid;
+
+reg  [3:0] 	cnt;
+initial begin
+    clk = 0;
+	cnt = 0;
+end
+always #5 clk = ~clk;
+
+initial begin
+    #44
+    rstn = 0;
+    #50
+    rstn = ~rstn;
+end
+
+always @(posedge clk) begin
+    cnt <= cnt + 1;
+end
+
+initial begin
+    data_in_valid = 0;
+end
+always @(posedge clk ) begin
+    if (cnt == 15)
+        data_in_valid <= 1;
+    else
+        data_in_valid <= 0;
+end
+
+integer k;
+initial begin
+	for (k = 0; k < CHANNEL_NUM; k=k+1) begin
+		bn_a[k] = $random;
+		bn_b[k] = $random;
+		res [k] = $random;
+	end
+end
+
+genvar i, j;
+generate
+    for (i = 0; i < CHANNEL_NUM; i=i+1) begin
+		always @(posedge clk ) begin
+			if (cnt == 15)
+				data_in[i] <= $random;
+			else
+				data_in[i] <= data_in[i];
+		end
+    end
+endgenerate
+
+bn_res #(
+	.DATA_WIDTH  (DATA_WIDTH ),
+	.PARA_WIDTH 	(PARA_WIDTH ),
+	.CHANNEL_NUM (CHANNEL_NUM)
+) u_bn_res(
+	.clk            (clk),
+	.rstn           (rstn),
+	.data_in_valid  (data_in_valid),
+	.data_in        (data_in),
+	.bn_a			(bn_a),
+	.bn_b			(bn_b),
+	.res			(res),
+	.data_out_valid (data_out_valid),
+	.data_out       (data_out)
+);
+
+endmodule
